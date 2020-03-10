4fb5fb0893b2b1974299d8de1752249c3ffb39f1
hkyang233 <1336551637@qq.com>
2020-03-10 16:15:15 +0800

    Site updated: 2020-03-10 16:15:14

---
+++ dist/music.js
@@ -0,0 +1,10 @@
+const ap = new APlayer({
+    container: document.getElementById('player'),
+    fixed: true,
+    audio: [{
+        name: 'Alone, Pt. II',
+        artist: 'Alan Walker',
+        url: 'demo.mp3',
+        cover: 'cover.jpg',
+    }]
+});
